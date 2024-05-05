2c0734bbf2a7181326818f5ecdd2793cca01c0d7
UnluckyCrafter <117037081+minidogg@users.noreply.github.com>
2024-05-05 15:02:48 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -12,13 +12,18 @@ using UnityEngine.UI;
 using UnityEngine.UIElements;
 using Steamworks.Data;
 using System.Runtime.InteropServices;
+using System.Drawing;
 
 
-namespace BoplBattleTemplate
+namespace FiberLib
 {
+    public class metadata
+    {
+        public static byte[] signature = [223, 145];
+    }
     [BepInPlugin(pluginGuid, "FiberLib", "1.0.0")]
     [BepInProcess("BoplBattle.exe")]
-    public class Plugin : BaseUnityPlugin
+    public class FiberLibPlugin : BaseUnityPlugin
     {
         public const string pluginGuid = "com.unluckycrafter.fiberlib";
         public static SteamManager curManager;
@@ -29,12 +34,12 @@ namespace BoplBattleTemplate
         {
 
             // Plugin startup logic
-            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_NAME} is loaded!");//feel free to remove this
+            Logger.LogInfo($"FiberLib is loaded!");//feel free to remove this
             Harmony harmony = new Harmony(pluginGuid);
 
             //fetch manager
             MethodInfo original3 = AccessTools.Method(typeof(SteamManager), "Update");
-            MethodInfo patch3 = AccessTools.Method(typeof(Plugin), "FetchManager");
+            MethodInfo patch3 = AccessTools.Method(typeof(FiberLibPlugin), "FetchManager");
             harmony.Patch(original3, new HarmonyMethod(patch3));
 
             //fetch socket
@@ -43,7 +48,7 @@ namespace BoplBattleTemplate
                         harmony.Patch(original5, new HarmonyMethod(patch5));*/
 
             MethodInfo original4 = AccessTools.Method(typeof(SteamSocket), "OnMessage");
-            MethodInfo patch4 = AccessTools.Method(typeof(Plugin), "OnMessageInterceptor");
+            MethodInfo patch4 = AccessTools.Method(typeof(FiberLibPlugin), "OnMessageInterceptor");
             harmony.Patch(original4, new HarmonyMethod(patch4));
         }
 
@@ -52,27 +57,19 @@ namespace BoplBattleTemplate
             curManager = ___instance;
             return true;
         }
-        public static bool FetchSocket(ref SteamSocket ___instance)
-        {
-            curSocket = ___instance;
-            return true;
-        }
-        public static void sendPacket()
-        {
 
-        }
-
-        public static byte[] messageBuffer = new byte[2048];
+        public static byte[] messageBuffer = new byte[1];
 
         public static bool OnMessageInterceptor(Connection connection, NetIdentity identity, IntPtr data, int size, long messageNum, long recvTime, int channel)
         {
             //setup for receiving packets
-            messageBuffer = new byte[2048];
+            messageBuffer = new byte[size];
             Marshal.Copy(data, messageBuffer, 0, size);
-            Enum packetEnum = NetworkTools.ReadUICom(messageBuffer).PacketTypeAsEnum;
+            if(!(messageBuffer[0] == metadata.signature[0] && messageBuffer[1] == metadata.signature[1]))return true;
 
             //handling packets
-            Console.WriteLine("Received a packet.");
+            Console.WriteLine("Custom packet received!");
+            
 
 
             //return to default method
@@ -84,9 +81,38 @@ namespace BoplBattleTemplate
             if (GUI.Button(new Rect(0, 50, 170f, 30f), "Send Packet"))
             {
                 Console.WriteLine("Sending packet!");
-
+                byte[] data = PacketCreator.constructPacket((byte[])[16,23], (byte[])[12]);
+                PacketCreator.distributePacket(curManager,data);
             }
         }
 
+    }
+    public class PacketCreator()
+    {
+        public static byte[] Combine(byte[] first, byte[] second, byte[] third)
+        {
+            byte[] ret = new byte[first.Length + second.Length + third.Length];
+            Buffer.BlockCopy(first, 0, ret, 0, first.Length);
+            Buffer.BlockCopy(second, 0, ret, first.Length, second.Length);
+            Buffer.BlockCopy(third, 0, ret, first.Length + second.Length,
+                             third.Length);
+            return ret;
+        }
+        public static byte[] constructPacket(byte[] pluginSignature, byte[] sendData)
+        {
+            int size = sendData.Length;
+            byte[] data;
+            data = Combine(metadata.signature,pluginSignature,sendData);
+
+            return data;
+        }
+        public static bool distributePacket(SteamManager manager, byte[] packet, SendType sendType = SendType.Reliable)
+        {
+            for(int i = 0;i< manager.connectedPlayers.Count; i++)
+            {
+                manager.connectedPlayers[i].Connection.SendMessage(packet,sendType);
+            }
+            return true;
+        }
     }
 }
