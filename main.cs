18dcb27077f3fdf5598a8a0b4e9faaa77af7c414
UnluckyCrafter <117037081+minidogg@users.noreply.github.com>
2024-05-05 13:55:12 -0500

    Update main.cs

--- main.cs
+++ main.cs
@@ -11,6 +11,7 @@ using BepInEx.Logging;
 using UnityEngine.UI;
 using UnityEngine.UIElements;
 using Steamworks.Data;
+using System.Runtime.InteropServices;
 
 
 namespace BoplBattleTemplate
@@ -37,19 +38,17 @@ namespace BoplBattleTemplate
             harmony.Patch(original3, new HarmonyMethod(patch3));
 
             //fetch socket
-/*            MethodInfo original5 = AccessTools.Method(typeof(SteamSocket), "Update");
-            MethodInfo patch5 = AccessTools.Method(typeof(Plugin), "FetchSocket");
-            harmony.Patch(original5, new HarmonyMethod(patch5));*/
+            /*            MethodInfo original5 = AccessTools.Method(typeof(SteamSocket), "Update");
+                        MethodInfo patch5 = AccessTools.Method(typeof(Plugin), "FetchSocket");
+                        harmony.Patch(original5, new HarmonyMethod(patch5));*/
 
-            //packet interceptor
             MethodInfo original4 = AccessTools.Method(typeof(SteamSocket), "OnMessage");
-            MethodInfo patch4 = AccessTools.Method(typeof(Plugin), "PacketInterceptor");
+            MethodInfo patch4 = AccessTools.Method(typeof(Plugin), "OnMessageInterceptor");
             harmony.Patch(original4, new HarmonyMethod(patch4));
         }
 
         public static bool FetchManager(ref SteamManager ___instance, ref SteamManager ___fields)
         {
-            ___fields.socket
             curManager = ___instance;
             return true;
         }
@@ -58,23 +57,34 @@ namespace BoplBattleTemplate
             curSocket = ___instance;
             return true;
         }
-
-        public static bool PacketInterceptor(Connection connection, NetIdentity identity, IntPtr data, int size, long messageNum, long recvTime, int channel)
+        public static void sendPacket()
         {
-            Console.WriteLine("Received packet: ");
-            Console.WriteLine("Data: "+data);
-            Console.WriteLine("Size: "+data);
 
+        }
 
+        public static byte[] messageBuffer = new byte[2048];
+
+        public static bool OnMessageInterceptor(Connection connection, NetIdentity identity, IntPtr data, int size, long messageNum, long recvTime, int channel)
+        {
+            //setup for receiving packets
+            messageBuffer = new byte[2048];
+            Marshal.Copy(data, messageBuffer, 0, size);
+            Enum packetEnum = NetworkTools.ReadUICom(messageBuffer).PacketTypeAsEnum;
+
+            //handling packets
+            Console.WriteLine("Received a packet.");
+    
+
+            //return to default method
             return true;
         }
 
         private void OnGUI()
         {
-            if(GUI.Button(new Rect(0,50,170f,30f),"Send Packet"))
+            if (GUI.Button(new Rect(0, 50, 170f, 30f), "Send Packet"))
             {
                 Console.WriteLine("Sending packet!");
-                
+
             }
         }
 
